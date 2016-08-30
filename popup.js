be76f6097d32545aa29d11ef1560ed19591736d6
Kyle Noble <kyle@531networks.com>
2016-08-30 12:08:00 -0700

    Added message logic and keyup updates

--- popup.js
+++ popup.js
@@ -1,22 +1,47 @@
-console.log('popup.js loaded')
-
-
-
-
 var setContainer = '1200px',
     setColNumber = 12,
-    setGutter    = 24;
+    setGutter    = 24,
+    overlayInit = false;
 
-document.getElementById('columns').addEventListener("change", function(){
-  setColNumber =  this.value
+document.getElementById('columns').addEventListener("keyup", function(){
+  var newValue = this.value;
+  updateValues('columns', newValue);
 });
 
 
+document.getElementById('gutter').addEventListener("keyup", function(){
+  var newValue = this.value;
+  updateValues('columns', newValue);
+});
+
+document.getElementById('turn_up').addEventListener("keyup", function(){
+  updateValues('check', null);
+});
 
 
+function updateValues(type, amount) {
+  // check if the overlay init is checked
+  overlayInit = $('#turn_up').is(':checked');
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {overlayInit: 'true', setContainer: setContainer, setGutter: setGutter, setColNumber}, function(response) {
-    // console.log(response.farewell);
+  if (type == 'columns') {
+    setColNumber = amount;
+  } else if (type == 'gutter') {
+    setGutter = amount;
+  } else if (type == 'check') {
+    if (!overlayInit) {
+      return;
+    }
+  }
+
+  // sends a message to the current tab with the updated values
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var port = chrome.tabs.connect(tabs[0].id);
+    port.postMessage({"tab": tabs[0].id,
+                      "overlayInit": overlayInit,
+                      "setContainer": setContainer,
+                      "setGutter": setGutter,
+                      "setColNumber": setColNumber}, function(response) {
+                        console.log(response.farewell);
+                      });
   });
-});
+}
