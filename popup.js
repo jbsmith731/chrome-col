3c796650ab618acf419d457b90de6269ee00e9d1
Kyle Noble <kyle@531networks.com>
2016-08-30 12:46:38 -0700

    Added persistence between popup open and close

--- popup.js
+++ popup.js
@@ -3,14 +3,32 @@ var setContainer = '1200px',
     setGutter    = 24,
     overlayInit = false;
 
+var values = chrome.storage.sync.get('currentValues', function(data) {
+  if (data.currentValues) {
+    overlayInit = data.currentValues.overlayInit;
+    setContainer = data.currentValues.setContainer;
+    setGutter = data.currentValues.setGutter;
+    setColNumber = data.currentValues.setColNumber;
+
+    $('#turn_up').prop('checked', overlayInit);
+    $('#containerWidth').val(setContainer);
+    $('#gutter').val(setGutter);
+    $('#columns').val(setColNumber);
+
+    if (data.currentValues.overlayInit) {
+        updateValues('check', null);
+    }
+  }
+});
+
 document.getElementById('columns').addEventListener("keyup", function(){
-  var newValue = this.value;
+  var newValue = parseInt(this.value);
   updateValues('columns', newValue);
 });
 
 
 document.getElementById('gutter').addEventListener("keyup", function(){
-  var newValue = this.value;
+  var newValue = parseInt(this.value);
   updateValues('gutter', newValue);
 });
 
@@ -35,6 +53,7 @@ function updateValues(type, amount) {
 
   // sends a message to the current tab with the updated values
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    saveCurrentValues();
     var port = chrome.tabs.connect(tabs[0].id);
     port.postMessage({"tab": tabs[0].id,
                       "overlayInit": overlayInit,
@@ -43,5 +62,25 @@ function updateValues(type, amount) {
                       "setColNumber": setColNumber}, function(response) {
                         console.log(response.farewell);
                       });
+  });
+}
+
+function saveCurrentValues() {
+  // Get a value saved in a form.
+  var theValue = {
+    "overlayInit": overlayInit,
+    "setContainer": setContainer,
+    "setGutter": setGutter,
+    "setColNumber": setColNumber
+  };
+  // Check that there's some code there.
+  if (!theValue) {
+    message('Error: No value specified');
+    return;
+  }
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set({'currentValues': theValue}, function() {
+    // Notify that we saved.
+    message('Settings saved');
   });
 }
